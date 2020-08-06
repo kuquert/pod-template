b7638f26f97d0015d2b4ad367161b176b722f28c
kuquert <marcus.kuquert@icloud.com>
2020-08-06 13:33:16 -0300

    Fix syntax that was causing the issue, not escape

--- setup/TemplateConfigurator.rb
+++ setup/TemplateConfigurator.rb
@@ -73,7 +73,8 @@ module Pod
       quit_xcode = self.ask_with_answers("Can I quit Xcode for you? This is going to help Xcode find itself after the `pod isntall` on the final step", ["Yes", "No"]).to_sym
 
       if quit_xcode == :yes
-          system "osascript -e 'quit app \"Xcode\"'"
+          system "osascript -e 'quit app ""Xcode""'"
+      end
 
       platform = self.ask_with_answers("What platform do you want to use?", ["iOS", "macOS"]).to_sym
 
