0081be982e9949feec59d88c79613d103c94914a
kuquert <marcus.kuquert@icloud.com>
2020-08-06 14:03:49 -0300

    Update variable name for clarity

--- setup/ConfigureSwift.rb
+++ setup/ConfigureSwift.rb
@@ -14,8 +14,8 @@ module Pod
     def perform
       keep_demo = configurator.ask_with_answers("Would you like to include a demo application with your library", ["Yes", "No"]).to_sym
 
-      framework = configurator.ask_with_answers("Which testing frameworks will you use", ["None", "Quick"]).to_sym
-      case framework
+      test_framework = configurator.ask_with_answers("Which testing frameworks will you use", ["None", "Quick"]).to_sym
+      case test_framework
         when :quick
           configurator.add_pod_to_podfile "Quick', '~> 1.2.0"
           configurator.add_pod_to_podfile "Nimble', '~> 7.0"
@@ -25,7 +25,7 @@ module Pod
           configurator.set_test_framework "xctest", "swift", "swift"
       end
 
-      snapshots = configurator.ask_with_answers("Would you like to do view based testing", ["No", "Yes"]).to_sym
+      snapshots = configurator.ask_with_answers("Would you like to do view based testing (FBSnapshotTestCase or Nimble-Snapshots)", ["No", "Yes"]).to_sym
       case snapshots
         when :yes
           configurator.add_pod_to_podfile "FBSnapshotTestCase' , '~> 2.1.4"
@@ -35,7 +35,7 @@ module Pod
               keep_demo = :yes
           end
 
-          if framework == :quick
+          if test_framework == :quick
               configurator.add_pod_to_podfile "Nimble-Snapshots' , '~> 6.3.0"
           end
       end
